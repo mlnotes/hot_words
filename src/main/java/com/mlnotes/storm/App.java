9786660cad486b5995983679de12c4acc7c390ca
Zhu Hanfeng <me@mlnotes.com>
2014-03-05 21:46:45 +0800

    submit to real cluster

--- src/main/java/com/mlnotes/storm/App.java
+++ src/main/java/com/mlnotes/storm/App.java
@@ -1,9 +1,13 @@
 package com.mlnotes.storm;
 
 import backtype.storm.Config;
-import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Hello world!
@@ -23,9 +27,17 @@ public class App
         // configuration
         Config config = new Config();
         // TODO set config
+        //LocalCluster cluster = new LocalCluster();
+            //cluster.submitTopology("word-count-topology", config, builder.createTopology());
         
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("word-count-topoloty", config, builder.createTopology());
-        
+        config.setNumWorkers(10);
+        config.setMaxSpoutPending(5000);
+        try {
+            StormSubmitter.submitTopology("word-count-topology", config, builder.createTopology());
+        } catch (AlreadyAliveException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidTopologyException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
